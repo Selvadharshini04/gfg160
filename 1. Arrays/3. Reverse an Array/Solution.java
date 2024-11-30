bcb7035f3baf200c386c452427d076e4da99ca31
Selvadharshini <selvadharshini12b@gmail.com>
2024-11-30 21:46:27 +0530

    Update Solution.java

--- 1. Arrays/3. Reverse an Array/Solution.java
+++ 1. Arrays/3. Reverse an Array/Solution.java
@@ -1,3 +1,13 @@
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Question:
+You are given an array of integers arr[]. Your task is to reverse the given array.
+
+Examples:
+
+Input: arr = [1, 4, 3, 2, 6, 5]
+Output: [5, 6, 2, 3, 4, 1]
+Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Solution {
     public void reverseArray(int arr[]) {
         // code here
